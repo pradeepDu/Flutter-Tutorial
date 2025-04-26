541e12742ee6d0b5eca300c09306000f3c372cac
pradeepDu <dukemarquis2004@gmail.com>
2025-04-26 20:42:18 +0530

    added listtile

--- lib/main.dart
+++ lib/main.dart
@@ -3,20 +3,52 @@ import "package:english_words/english_words.dart";
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-   
+    
     return MaterialApp(
       theme: ThemeData(
         primaryColor: Colors.blue[200],
-        appBarTheme: AppBarTheme(backgroundColor: Colors.blue[200],)
+        appBarTheme: AppBarTheme(backgroundColor: Colors.blue[200]),
       ),
-      home: Scaffold(
-        appBar:AppBar(title: Text("Nash Femboi")) ,
-        body: Center(child:Text("Suck mah nuts Nes")),
+      home: RandomWords(),
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  final _randomWordPair = <WordPair>[];
+  Widget _buildList() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, item) {
+        if (item.isOdd) return  Divider();
+        final i = item ~/ 2;
+        if (i >= _randomWordPair.length) {
+          _randomWordPair.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_randomWordPair[i]);
+      },
+    );
+  }
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: TextStyle(fontSize: 18.0),
       ),
     );
   }
-
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Nes Femboi')),
+      body: _buildList(),
+    );
+  }
 }
